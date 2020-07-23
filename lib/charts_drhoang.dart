450927a889028d127fbf591a0689d11aed98a21d
DESKTOP-6DCILKM\POSAPP-IT <drhoang999@gmail.com>
2020-07-23 11:24:41 +0700

    Change class

--- lib/charts_drhoang.dart
+++ lib/charts_drhoang.dart
@@ -1,7 +1,7 @@
 library charts_drhoang;
 
 /// A Calculator.
-class Calculator {
+class Calculator222 {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
--- test/charts_drhoang_test.dart
+++ test/charts_drhoang_test.dart
@@ -4,7 +4,7 @@ import 'package:charts_drhoang/charts_drhoang.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
+    final calculator = Calculator222();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
